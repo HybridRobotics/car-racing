a5e73f5c3f6b28d5f2e171559d75efb3de271425
Jun Zeng <zengjunsjtu@berkeley.edu>
2021-01-27 00:08:59 -0800

    Add tests

--- tests/identification.py
+++ tests/identification.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import racing_env, racing_car, racing_sim, policy, utils
 
 
-def linear_time_invariant_identification():
+def linear_time_invariant():
     # define closed_track
     track_spec = np.array(
         [
@@ -45,27 +45,6 @@ def linear_time_invariant_identification():
     print(matrix_B)
 
 
-def mpc_tracking():
-    track_spec = np.genfromtxt("data/track_spec/default.csv", delimiter=",")
-    track_width = 1.0
-    track = racing_env.ClosedTrack(track_spec, track_width)
-    matrix_A = np.genfromtxt("data/sys/LTI/matrix_A.csv", delimiter=",")
-    matrix_B = np.genfromtxt("data/sys/LTI/matrix_B.csv", delimiter=",")
-    matrix_Q = np.diag([1.0, 1.0, 1.0, 1.0, 0.0, 100.0])
-    matrix_R = np.diag([1.0, 10.0])
-    # setup ego car
-    ego = racing_car.DynamicBicycleModel(
-        name="ego", car_param=racing_car.CarParam(color="r"), x=np.zeros((6,)), x_glob=np.zeros((6,)),
-    )
-    ego.set_ctrl_policy(policy.MPCSpeedTracking(matrix_A, matrix_B, matrix_Q, matrix_R, vt=0.6))
-    # setup simulation
-    simulator = racing_sim.CarRacingSim()
-    simulator.set_timestep(0.1)
-    simulator.set_track(track)
-    simulator.add_vehicle(ego)
-    simulator.sim(sim_time=100.0)
-
-
 if __name__ == "__main__":
-    # linear_time_invariant_identification()
-    mpc_tracking()
+    linear_time_invariant()
+
---
+++ tests/tracking.py
@@ -0,0 +1,38 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import racing_env, racing_car, racing_sim, policy, utils
+
+
+def tracking(args):
+    track_spec = np.genfromtxt("data/track_spec/default.csv", delimiter=",")
+    track_width = 1.0
+    track = racing_env.ClosedTrack(track_spec, track_width)
+    matrix_A = np.genfromtxt("data/sys/LTI/matrix_A.csv", delimiter=",")
+    matrix_B = np.genfromtxt("data/sys/LTI/matrix_B.csv", delimiter=",")
+    matrix_Q = np.diag([1.0, 1.0, 1.0, 1.0, 0.0, 100.0])
+    matrix_R = np.diag([1.0, 10.0])
+    # setup ego car
+    ego = racing_car.DynamicBicycleModel(
+        name="ego", car_param=racing_car.CarParam(color="r"), x=np.zeros((6,)), x_glob=np.zeros((6,)),
+    )
+    if args["ctrl_policy"] == "pid":
+        ego.set_ctrl_policy(policy.PIDSpeedTracking(vt=0.6))
+    elif args["ctrl_policy"] == "mpc-lti":
+        ego.set_ctrl_policy(policy.MPCSpeedTracking(matrix_A, matrix_B, matrix_Q, matrix_R, vt=0.6))
+    else:
+        raise NotImplementedError
+    # setup simulation
+    simulator = racing_sim.CarRacingSim()
+    simulator.set_timestep(0.1)
+    simulator.set_track(track)
+    simulator.add_vehicle(ego)
+    simulator.sim(sim_time=100.0)
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--ctrl_policy", type=str, default="pid")
+    args = vars(parser.parse_args())
+    tracking(args)
